f9b1e787b73101f5f0fcfb994eb72bf209c30c44
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-04 13:48:50 +0000

    maint: Adjust fields in admin panel

--- gamecrit_post/admin.py
+++ gamecrit_post/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('post_name', 'status')
+    search_fields = ['post_name',]
+    list_filter = ('status',)
+    summernote_fields = ('post_field',)
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)

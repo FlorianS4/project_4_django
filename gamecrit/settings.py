d7ebcb0189a135f66ec13a0c4b87b60e4e20b1d0
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-05 12:11:46 +0000

    feat: Add code institute index.html to add basic structure (will change later)

--- gamecrit/settings.py
+++ gamecrit/settings.py
@@ -18,7 +18,8 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'gamecrit_post', 'templates', 'gamecrit_post')
+TEMPLATES_DIR = [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'gamecrit_post', 'templates', 'gamecrit_post'),]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'gamecrit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': TEMPLATES_DIR,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gamecrit_post/views.py
+++ gamecrit_post/views.py
@@ -6,4 +6,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("created_on")
-    template_name = "gamecrit_post_list.html"
+    template_name = "gamecrit_post/index.html"
+    paginate_by = 6

b11a9cc71891b0d78fa63cd6acdd7b986604ff88
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-06 16:10:21 +0000

    maint: Add class to views.py in contact app

--- gamecrit_contact/views.py
+++ gamecrit_contact/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from .models import GameCritContact
 
 # Create your views here.
+
+class GameCritContactList(generic.ListView):
+    queryset = GameCritContact.objects.all().order_by("created_on")
+    template_name = "gamecrit_contact/gamecrit_contact.html"
+    paginate_by = 3
+

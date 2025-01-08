21a3319afb84e1bd57724f25b2b0f668b39d3123
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-08 13:19:42 +0000

    feat: Add Django allauth package

--- gamecrit/urls.py
+++ gamecrit/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('gamecrit_contact/', include('gamecrit_contact.urls'), name='gamecrit_contact_urls'),
     path('summernote/', include('django_summernote.urls')),
     path("", include("gamecrit_post.urls"), name="gamecrit_post-urls"),

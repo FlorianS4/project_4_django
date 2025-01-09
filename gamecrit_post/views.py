23de27e5bce42b80398d40b94580ca7c9572f965
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-09 17:36:09 +0000

    feat: made comments visiable on gamecrit site

--- gamecrit_post/views.py
+++ gamecrit_post/views.py
@@ -25,9 +25,14 @@ def display_game_review(request, slug):
 
     queryset = Post.objects.filter(status=1)
     gamecrit_post = get_object_or_404(queryset, slug=slug)
+    comments = gamecrit_post.comments.all().order_by("-created_on")
+    comment_count = gamecrit_post.comments.filter(approved=True).count()
 
     return render(
         request,
         "gamecrit_post/display_game_review.html",
-        {"gamecrit_post": gamecrit_post},
+        {"gamecrit_post": gamecrit_post,
+        "comments": comments,
+        "comment_count": comment_count,
+        },
     )

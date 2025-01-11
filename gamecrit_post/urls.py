402f327fe1f7fc2305274ac2df9c35ee5c747fb6
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-11 17:24:09 +0000

    maint: Update path in urls

--- gamecrit_post/urls.py
+++ gamecrit_post/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.display_game_review, name='display_game_review'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]

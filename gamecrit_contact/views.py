ffce96abb9f84ec588488e29849bdb6cf9d729a7
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-11 17:04:18 +0000

    maint: Add POST request to gamecrit_contact form

--- gamecrit_contact/views.py
+++ gamecrit_contact/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import GameCritContact
 from .forms import GameCritContactForm
 
@@ -8,6 +9,12 @@ def gamecrit_contact(request):
     """
     User can get in contact with site owner
     """
+
+    if request.method == "POST":
+        contact_form = GameCritContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, "Hey, thanks for your message. I will check it out and contact you in the next 3 days.")
     contact_form = GameCritContactForm()
 
     return render(

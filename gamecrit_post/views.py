da887b726acc33db529695e9736644cce68ff8be
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-11 17:21:10 +0000

    feat: Add feature to update comment to views

--- gamecrit_post/views.py
+++ gamecrit_post/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
 from django.contrib import messages
-from .models import Post
+from django.http import HttpResponseRedirect
+from .models import Post, Comment
 from .forms import GameCritCommentForm
 
 # Create your views here.
@@ -53,3 +54,25 @@ def display_game_review(request, slug):
         "comment_form": comment_form,
         },
     )
+
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        gamecrit_post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.username == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post_id = gamecrit_post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))  

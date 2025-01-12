5060a3611de1f46f04bde370f6c1df8c041be538
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-12 08:55:00 +0000

    maint: Add delete method to views

--- gamecrit_post/views.py
+++ gamecrit_post/views.py
@@ -76,3 +76,19 @@ def comment_edit(request, slug, comment_id):
 
     return HttpResponseRedirect(reverse('display_game_review', args=[slug]))
 
+
+def comment_delete(request, slug, comment_id):
+    """
+    view to delete comments
+    """
+    queryset = Post.objects.filter(status=1)
+    gamecrit_post = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if comment.username == request.user:
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, 'Your comment was deleted!')
+    else:
+        messages.add_message(
+            request, messages.ERROR, 'You can only delete your own comments!')
+    return HttpResponseRedirect(reverse('display_game_review', args=[slug]))
+

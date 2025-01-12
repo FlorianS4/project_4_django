1e57b0cddfbc5796b8593e911c138772c6b96da1
Florian Sassmann <florian.sassmann@hotmail.com>
2025-01-12 09:10:14 +0000

    feat: Add Javascript delete ability and add delete modul for UX

--- gamecrit_post/urls.py
+++ gamecrit_post/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.display_game_review, name='display_game_review'),
     path("<slug:slug>/comment_edit/<int:comment_id>/", views.comment_edit, name='comment_edit'),
+    path("<slug:slug>/delete_comment/<int:comment_id>/", views.comment_delete, name='comment_delete'),
 ]
